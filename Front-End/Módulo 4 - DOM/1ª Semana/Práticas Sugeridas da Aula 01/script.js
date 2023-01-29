2bb5f8d7519213a0be253a32a6c31ee8ce0631a7
aylmerbolzan <aylmer.bolzan@gmail.com>
2023-01-29 05:46:16 -0300

    Execução da 1ª Semana - DOM

---
+++ Front-End/Módulo 4 - DOM/1ª Semana/Práticas Sugeridas da Aula 01/script.js
@@ -0,0 +1,10 @@
+var corpo = document.body;
+
+corpo.style.background = "Gray";
+
+document.title = "Novo Título";
+
+
+var h1_brasil = document.getElementById(#pais_brasil);
+h1_brasil.style.background = "Yellow";
+document.write ("<p> innerText: " + h1_brasil.innerText);
---
+++ Front-End/Módulo 4 - DOM/1ª Semana/Trabalho Prático da 1ª Semana/script.js
@@ -0,0 +1,14 @@
+// Selecionar todos elementos da classe 'produto_preco'
+
+let soma = [];
+
+let precos = document.getElementsByClassName("produto_preco");
+soma.push();
+
+document.write(soma);
+document(precos);
+document.write("precowefwews");
+
+// Implementar um comando de repetição para analisar o valor 'innerText' de cada um dos elementos e somá-los (lembre-se de converter o valor para float)
+
+// Escrever no conteúdo da página o valor da soma
