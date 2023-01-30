dff1f5a2a3c8246bafdf78f6dfe4ea3c68d37752
aylmerbolzan <aylmer.bolzan@gmail.com>
2023-01-30 05:35:20 -0300

    Resolução da atividade da 1º Semana de DOM

--- Front-End/Módulo 4 - DOM/1ª Semana/Trabalho Prático da 1ª Semana/script.js
+++ Front-End/Módulo 4 - DOM/1ª Semana/Trabalho Prático da 1ª Semana/script.js
@@ -1,14 +1,14 @@
 // Selecionar todos elementos da classe 'produto_preco'
 
-let soma = [];
-
 let precos = document.getElementsByClassName("produto_preco");
-soma.push();
-
-document.write(soma);
-document(precos);
-document.write("precowefwews");
 
 // Implementar um comando de repetição para analisar o valor 'innerText' de cada um dos elementos e somá-los (lembre-se de converter o valor para float)
 
+let soma = 0;
+for (var i = 0; i < precos.length; i++) {
+  soma += parseFloat(precos[i].innerText);
+}
+
 // Escrever no conteúdo da página o valor da soma
+
+document.write("Total: R$ " + soma);
