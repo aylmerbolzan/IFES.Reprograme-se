54f4c5dd5eaefb047451382f693bab262ac04cb3
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-04-17 19:14:05 -0300

    Configurações iniciais do P1

--- FIC2 - Back-End/Módulo 2 - Páginas customizadas com Node.JS/2ª Semana/Trabalho Prático da 2ª Semana/index.js
+++ FIC2 - Back-End/Módulo 2 - Páginas customizadas com Node.JS/2ª Semana/Trabalho Prático da 2ª Semana/index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 const porta = 443;
 
-app.get("/P1", function (req, res) {
-  res.send("P1");
+app.get('/P1', function (req, res) {
+  res.send(__dirname + "/p1.html");
 });
 
-app.get("/P2", function (req, res) {
-  res.send("P2");
-}); 
+app.get('/P2', function (req, res) {
+  res.send(__dirname + "/p2.html");
+});
 
 app.get("/P3", function (req, res) {
   res.send("P3");
-}); 
+});
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/home.html");
