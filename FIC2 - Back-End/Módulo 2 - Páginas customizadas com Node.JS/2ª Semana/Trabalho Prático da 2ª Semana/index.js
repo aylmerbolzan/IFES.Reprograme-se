db1879166bf8be608fa985a3b2a21375cc786107
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-04-17 21:30:55 -0300

    Execução da Atividade da Semana 02 do Módulo 02

--- FIC2 - Back-End/Módulo 2 - Páginas customizadas com Node.JS/2ª Semana/Trabalho Prático da 2ª Semana/index.js
+++ FIC2 - Back-End/Módulo 2 - Páginas customizadas com Node.JS/2ª Semana/Trabalho Prático da 2ª Semana/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 const porta = 443;
 
-app.get('/P1', function (req, res) {
-  res.send(__dirname + "/p1.html");
+app.get("/P1", function (req, res) {
+  res.sendFile(__dirname + "/p1.html");
 });
 
-app.get('/P2', function (req, res) {
-  res.send(__dirname + "/p2.html");
+app.get("/P2", function (req, res) {
+  res.sendFile(__dirname + "/p2.html");
 });
 
 app.get("/P3", function (req, res) {
-  res.send("P3");
+  res.sendFile(__dirname + "/p3.html");
 });
 
 app.get("/", function (req, res) {
