69a01ab8a7b70f705d48bf6fa7270e9f698eab89
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-04-29 22:00:50 -0300

    Aula 11 - Upload de arquivos com formidable - Pt.2

--- FIC2 - Back-End/Módulo 2 - Páginas customizadas com Node.JS/3ª Semana/Aula 11 - Upload de arquivos com o módulo formidable/index.js
+++ FIC2 - Back-End/Módulo 2 - Páginas customizadas com Node.JS/3ª Semana/Aula 11 - Upload de arquivos com o módulo formidable/index.js
@@ -4,38 +4,34 @@ const formidavel = require("formidable");
 const fs = require("fs");
 
 const servidor = http.createServer((req, res) => {
- if (req.url !=enviodearquivo){
+  if (req.url != "/enviodearquivo") {
     res.writeHead(200, { "Content-Type": "text/html" });
-res.write(
-  '<form action="enviodearquivo" method="post" enctype="multipart/form-data">'
-);
-res.write('<input type="file" name="fileupload"><br>');
-res.write('<input type="submit" value="Enviar">');
-res.write("</form>");
-return res.end();
-
- } else {
-    const form = new formidable.IncomingForm();
+    res.write(
+      '<form action="enviodearquivo" method="post" enctype="multipart/form-data">'
+    );
+    res.write('<input type="file" name="fileupload"><br>');
+    res.write('<input type="submit" value="Enviar">');
+    res.write("</form>");
+    return res.end();
+  } else {
+    const form = new formidavel.IncomingForm();
     form.parse(req, (erro, campos, arquivos) => {
+      const urlAntiga = arquivos.fileupload.filepath;
+      const urlNova =
+        "./enviodearquivo/" + arquivos.fileupload.originalFilename;
 
-      const urlAntiga = arquivos.filetoupload.filepath;
-      const urlNova = "./enviodearquivo" + arquivos.filetoupload.originalFilename;
+      var rawData = fs.readFileSync(urlAntiga);
+      fs.writeFile(urlNova, rawData, function (err) {
+        if (err) console.log(err);
 
-
-    var rawData = fs.readFileSync(urlAntiga);
-    
+        res.write("Arquivo enviado com sucesso!");
+        res.end();
       });
     });
   }
- }
-
-
-
 });
 
 servidor.listen(porta, () => {
   console.log(`Servidor rodando em http://localhost:${porta}`);
 });
-
-
 
