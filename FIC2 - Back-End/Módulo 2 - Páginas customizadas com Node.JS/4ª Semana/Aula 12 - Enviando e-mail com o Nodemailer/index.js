cec63c5d32b734b0489efe401c381b574921b458
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-04-30 21:49:20 -0300

    Aula 12 - Enviando e-mail com o Nodemailer

---
+++ FIC2 - Back-End/Módulo 2 - Páginas customizadas com Node.JS/4ª Semana/Aula 12 - Enviando e-mail com o Nodemailer/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const nodemailer = require("nodemailer");
+const porta = 443;
+
+app.get("/", (req, res) => {
+  res.send("Enviando e-mail com o Nodemailer!");
+});
+
+app.get("/sendmail", async (req, res) => {
+  var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "a59d75b9a91dd2",
+      pass: "e3c4437e6efd52",
+    },
+  });
+
+  var message = {
+    from: "aylmer.bolzan@gmail.com",
+    to: "aylmerbolzan@gmail.com",
+    subject: "Message title",
+    text: "Plaintext version of the message",
+    html: "<p>HTML version of the message</p>",
+  };
+
+  transport.sendMail(message, function (err) {
+    if (err)
+      return res.status(400).json({
+        erro: true,
+        mensagem: "Erro: email não enviado",
+      });
+    else {
+      return res.json({
+        erro: false,
+        mensagem: "Email enviado com sucesso!",
+      });
+    }
+  });
+});
+
+app.listen(porta, () => {
+  console.log("Servidor rodando");
+});
