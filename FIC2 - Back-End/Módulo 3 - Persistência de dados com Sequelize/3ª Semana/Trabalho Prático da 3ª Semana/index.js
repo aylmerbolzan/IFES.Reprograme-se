052d63668bfec0306e2a9de9528ad7b5d776e038
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-05-18 23:12:00 +0000

    feat: front-end

--- FIC2 - Back-End/Módulo 3 - Persistência de dados com Sequelize/3ª Semana/Trabalho Prático da 3ª Semana/index.js
+++ FIC2 - Back-End/Módulo 3 - Persistência de dados com Sequelize/3ª Semana/Trabalho Prático da 3ª Semana/index.js
@@ -1,58 +1,59 @@
 /// criando o servidor
-const express = require('express');
+const express = require("express");
 const app = express();
 const porta = 9443;
 
-const bodyParser = require('body-parser');
-const database = require('./db');
-const Cliente = require('./models/cliente')
-const Fornecedor = require('./models/fornecedor')
+const bodyParser = require("body-parser");
+const database = require("./db");
+const Cliente = require("./models/cliente");
+const Fornecedor = require("./models/fornecedor");
 
 // Setar os valores da view e engine
-app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
+app.set("view engine", "html");
+app.engine("html", require("ejs").renderFile);
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Definindo rotas
-app.get('/', (req, res) =>{
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get("/cadcliente", function (req, res){
-  res.render('formCliente');
+app.get("/cadcliente", function (req, res) {
+  res.render("formCliente");
 });
 
-app.get("/cadfornecedor", function (req, res){
-  res.render('formFornecedor');
+app.get("/cadfornecedor", function (req, res) {
+  res.render("formFornecedor");
 });
 
-app.post("/addcliente", function (req, res){
+app.post("/addcliente", function (req, res) {
   Cliente.create({
     nome: req.body.nome,
     nascimento: req.body.nascimento,
     cidade: req.body.cidade,
     telefone: req.body.telefone,
-  }).then(function(){
-    res.send("Cliente cadastrado com sucesso");
-  })
-})
+  }).then(function () {
+    res.render("cadastroSucesso");
+  });
+});
 
-app.post("/addfornecedor", function (req, res){
+app.post("/addfornecedor", function (req, res) {
   Fornecedor.create({
     nome: req.body.nome,
     telefone: req.body.telefone,
     email: req.body.email,
-  }).then(function(){
-    res.send("Fornecedor cadastrado com sucesso");
-  })
-})
+  }).then(function () {
+    res.render("cadastroSucesso");
+  });
+});
 
-app.listen(porta, () => { console.log('Servidor rodando') });
+app.listen(porta, () => {
+  console.log("Servidor rodando");
+});
 
-
-(async() => {
-  try{
+(async () => {
+  try {
     const resultado = await database.sync();
     console.log(resultado);
     const clientes = await Cliente.findAll();
@@ -60,7 +61,7 @@ app.listen(porta, () => { console.log('Servidor rodando') });
 
     const fornecedores = await Fornecedor.findAll();
     console.log("Lista de fornecedores \n", fornecedores);
-  } catch(error){
-    console.log(error)
+  } catch (error) {
+    console.log(error);
   }
 })();
