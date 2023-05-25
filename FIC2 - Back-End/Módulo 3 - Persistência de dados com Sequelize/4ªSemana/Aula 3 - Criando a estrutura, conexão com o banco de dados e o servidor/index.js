ee01ad359a2b5ddbbd46ff3a8a0f939f010a2dd5
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-05-24 22:11:57 -0300

    Aula 3 - Criando a estrutura, conexao com o banco de dados e o servidor

---
+++ FIC2 - Back-End/Módulo 3 - Persistência de dados com Sequelize/4ªSemana/Aula 3 - Criando a estrutura, conexão com o banco de dados e o servidor/index.js
@@ -0,0 +1,17 @@
+//Bibliotecas/módulos utilizados
+const database = require('./db/db');
+const express = require('express');
+const app = express();
+
+
+//Sincronismo do banco de dados
+try {
+    database.sync().then(()=>{
+app.listen(9443, ()=>{
+    console.log('Servidor rodando na porta 9443');
+})
+    });
+
+} catch (error){
+    console.log('Não foi possível sincronizar o banco de dados:', error);
+}
